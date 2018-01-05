be389c9d568b24fc740709943b8e0d49c70ce411
Michal Krzyzanowski <mich.krzyzanowski@gmail.com>
2018-01-05 03:42:05 +0100

    Comments working

---
+++ public/js/main.js
@@ -0,0 +1,72 @@
+
+// Copyright: Michael Rose (mmistakes)
+// https://github.com/mmistakes/made-mistakes-jekyll/blob/master/src/assets/javascripts/main.js
+// Static comments
+(function($) {
+  var $comments = $(".js-comments");
+
+  $("#comment-form").submit(function() {
+    var form = this;
+
+    $("#comment-form-submit").html(
+      '<span class="fas fa-spinner fa-spin"></span> Loading...'
+    );
+
+    $.ajax({
+      type: $(this).attr("method"),
+      url: $(this).attr("action"),
+      data: $(this).serialize(),
+      contentType: "application/x-www-form-urlencoded",
+      success: function(data) {
+        $("#comment-form-submit")
+          .html("Submitted")
+          .addClass("btn-disabled");
+        $("#comment-form .js-notice")
+          .removeClass("notice-danger")
+          .addClass("notice-success");
+        $("#comment-form .js-notice-icon")
+          .removeClass("fa-exclamation-triangle")
+          .addClass("fa-thumbs-up");
+        showAlert(
+          '<strong>Thanks for your comment!</strong> It is currently pending a review and will show on the site once approved.'
+        );
+      },
+      error: function(err) {
+        console.log(err);
+        $("#comment-form-submit").html("Comment");
+        $("#comment-form .js-notice")
+          .removeClass("notice-success")
+          .addClass("notice-danger");
+        $("#comment-form .js-notice-icon")
+          .removeClass("fa-thumbs-up")
+          .addClass("fa-exclamation-triangle");
+        showAlert(
+          "<strong>Sorry, there was an error with your submission.</strong> Please make sure all required fields have been completed and try again."
+        );
+      }
+    });
+
+    return false;
+  });
+
+  function showAlert(message) {
+    $("#comment-form .js-notice").removeClass("hidden");
+    $("#comment-form .js-notice-text").html(message);
+  }
+})(jQuery);
+
+(function($) {
+    $(".comment-reply a").click(function() {
+      $("#cancel-comment-reply-link")
+        .removeClass("hidden")
+        .append("")
+        .append($(this).data("name"));
+      $("#comment-parent").val($(this).data("index"));
+    });
+
+    $("#cancel-comment-reply-link").click(function() {
+      $(this).addClass("hidden");
+      $("#comment-parent").val("");
+    });
+}
+)(jQuery);
